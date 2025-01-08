968740bc54cbac56f6db95fdc389d14cfe9a819d
YESWANTH <113580288+yeswanth6463@users.noreply.github.com>
2025-01-08 10:21:46 +0530

    c

--- advance.py/superdigit.py
+++ advance.py/superdigit.py
@@ -18,3 +18,7 @@ class my_sups:
 m1 = my_sups()
 rest = m1.my_method(True, 10, 'pusp', 23, 33.46, 435, [1, 2, 3, 4, 5], 67, False)
 print(rest)  # Output: 4
+print(rest)  # Output: 4
+print(rest)  # Output: 4
+print(rest)  # Output: 4
+print(rest)  # Output: 4
